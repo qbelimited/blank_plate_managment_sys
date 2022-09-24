e9fb4574c203bc2d779f15f32be276f0a970b1d3
Emmanuel Kwaku Botchway <emmlquake@live.com>
2022-09-24 17:40:39 +0000

    Update User.php
    Adding company relationship

--- app/Models/User.php
+++ app/Models/User.php
@@ -59,4 +59,9 @@ class User extends Authenticatable
             get: fn ($value) =>  ["admin", "manufacturer", "dvla", "embosser"][$value],
         );
     }
+
+    public function company()
+    {
+        return $this->belongsTo('App\Company');
+    }
 }
