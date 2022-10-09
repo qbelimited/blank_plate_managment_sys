f4605b0602c17b0cdfb5967799aeb2536deec0ee
unionsghr <hr@unionsg.com>
2022-10-09 00:03:13 +0000

    Get all users
    A get all users and their roles API has been added

---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+use Spatie\Permission\Models\Role;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+
+class UserController extends Controller
+{
+    //update user function
+    public function updateUser(Request $request){
+        
+        //check for permission
+        // $user = User::find($request->posted_by);
+        // if(!$user->hasRole('Admin')){
+        //     return response()->json(['response_code'=>'401','message'=>'user does not have permission to create users']);
+        // }
+
+        //check for permission
+        if(!Auth::user()->hasRole('Admin')){
+            return response()->json(['response_code'=>'401','message'=>'user does not have permission to update users']);
+        }
+
+
+        //validate user entry
+        $validator = Validator::make($request->all(), [
+            'fname' => 'required',
+            'national_id' => [
+                'required',
+                Rule::unique('users')->ignore($request->id),
+            ],
+            'role_id' => 'required',
+            'email' => [
+                'required',
+                Rule::unique('users')->ignore($request->id),
+            ],
+        ]);
+
+        //if the validation fails return error
+        if($validator->fails()){
+            return $validator->messages();
+        }else{
+
+            //get the user 
+            $user = User::find($request->id);
+
+            $user->fname = $request->fname;
+            $user->lname = $request->lname;
+            $user->email = $request->email;
+            $user->national_id = $request->national_id;
+            $user->company_id = $request->company_id;
+
+            $ok = $user->save();
+
+            //assign role to user
+            $role = Role::find($request->role_id);
+            $user->syncRoles($role);
+
+            if($ok){
+                return response()->json(['response_code'=>'200','message'=>'user updated successfully']);
+            }
+
+        }
+
+
+        
+
+    }
+
+   
+    //this function gets all users and their roles
+    public function getUsers(){
+
+        //check for permission
+        if(!Auth::user()->hasRole('Admin')){
+            return response()->json(['response_code'=>'401','message'=>'user does not have permission to view users']);
+        }
+
+        return response()->json(['users' => User::with('roles')->paginate(15),'response_code'=>'200','message'=>'All users']);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\UserController;
 use App\Http\Controllers\Api\LoginController;
 use App\Http\Controllers\Api\RegisterController;
 
@@ -30,7 +31,8 @@ Route::prefix('/user/v1')->group(function(){
     Route::post('/login', [LoginController::class, 'login']);
     Route::group(['middleware' => ['auth:api']], function () {
         Route::post('/register', [RegisterController::class, 'register']);
-        Route::post('/update-user', [RegisterController::class, 'updateUser']);
+        Route::post('/update-user', [UserController::class, 'updateUser']);
+        Route::get('/get-users', [UserController::class, 'getUsers']);
         
     });
     
